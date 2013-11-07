fa0bea93ec329922eb7e891439775e627780f3e5
pastak <pasta0915@gmail.com>
2013-11-07 20:01:48 +0900

    直した

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 NAME="ShutoMORII"
 JNAME="森井 崇斗"
 NUMBER="1029-25-2723"
-PDFILENAME="report.pdf"
+PDFFILENAME="report.pdf"
 mv $1/${PDFFILENAME} $1/${NUMBER}-${NAME}-$1.pdf
 open "mailto:sicp-$1@kuis.kyoto-u.ac.jp?subject=アルゴリズムとデータ構造入門 第$1回 課題提出&body=所属：工学部 情報学科
 氏名：${JNAME}
