b071204a93185014c06fb6e14fffb39799710931
pastak <pasta0915@gmail.com>
2013-11-07 20:00:50 +0900

    練習問題1.29を解いた

---
+++ 5/1-29.scm
@@ -0,0 +1,30 @@
+(define (simpson-iter f a b n)
+(let* ((h (/ (- b a) n)) 
+       (y (lambda (k) (f (+ a (* k h)))))
+      )
+  (define (iter count sum)
+   (if (= count n) 
+    (+ sum (y n))
+    (iter (+ count 1) (+ sum (* (+ 2 (* 2(remainder count 2))) (y count))))
+   )
+  )
+  (* (/ h 3) (iter 1 0) )
+ )
+ )
+
+(simpson-iter (lambda (x) (* x x x)) 0 1 100)
+(simpson-iter (lambda (x) (* x x x)) 0 1 1000)
+
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
